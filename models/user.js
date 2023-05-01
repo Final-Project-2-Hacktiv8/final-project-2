bcf3592105c7e3c55cf57edf2b47899ec4d42967
Krisna Lavendra <krisnalavendrairawan@gmail.com>
2023-05-02 00:50:44 +0700

    add helpers jwt and bcrypt

--- models/user.js
+++ models/user.js
@@ -2,13 +2,10 @@
 const {
   Model
 } = require('sequelize');
+const { hashPassword } = require('../helpers/bcrypt')
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       User.hasMany(models.Photo, {foreignKey: 'userId'})
       User.hasMany(models.Comment, {foreignKey: 'userId'})
@@ -96,6 +93,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    hooks : {
+      beforeCreate : (user, options) => {
+        user.password = hashPassword(user.password)
+      }
+    }
   });
   return User;
 };
