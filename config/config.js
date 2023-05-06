67fa488b82d8f44d5a8fe2e270e9d2a26185d9d0
Krisna Lavendra <krisnalavendrairawan@gmail.com>
2023-05-06 16:27:04 +0700

    clean code

--- config/config.js
+++ config/config.js
@@ -13,13 +13,13 @@ const config = {
     url : process.env.DB_URL
   },
   production: {
-    username: process.env.DB_USERNAME_PROD,
-    password: process.env.DB_PASSWORD_PROD,
-    database: process.env.DB_NAME_PROD,
-    host: process.env.DB_HOST_PROD,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
-    port: process.env.DB_PORT_PROD,
-    url : process.env.DB_URL_PROD
+    port: process.env.DB_PORT,
+    url : process.env.DB_URL
   }
 }
 
