aa41024333c0c4f44f295a517e1d2dc33c0e86e9
XvcreAm <47131367+XvcreAm@users.noreply.github.com>
2023-05-10 18:19:38 +0700

    Authorization for Comment

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,4 +1,4 @@
-const {User, Photo, SocialMedia} = require("../models");
+const {User, Photo, SocialMedia, Comment} = require("../models");
 
 
 //user authorization
@@ -93,9 +93,40 @@ function socialmediaAuthorization (req, res, next) {
     })
 }
 
+function commentAuthorization (req, res, next) {
+    const id = req.params.id
+    Comment.findOne({
+        where: {
+            id: id
+        }
+    })
+    .then(data => {
+        if(!data){
+            return res.status(404).json({
+                message: 'Comment not found',
+                devMessage: `Comment with id ${id} not found`
+            })
+        }
+        if(data.UserId !== res.locals.user.id){
+            return res.status(401).json({
+                message: 'User not authorized',
+                devMessage: `User with id ${res.locals.user.id} not authorized to id ${id}`
+            })
+        }
+        return next();
+    })
+    .catch(err => {
+        return res.status(500).json({
+            message: 'Internal Server Error',
+            devMessage: err
+        })
+    })
+}
+
 
 module.exports = {
     userAuthorization,
     photoAuthorization,
-    socialmediaAuthorization
+    socialmediaAuthorization,
+    commentAuthorization
 }
